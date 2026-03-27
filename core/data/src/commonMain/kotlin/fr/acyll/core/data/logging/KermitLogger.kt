7abe14622ea7fdba5558d833a9d8239c45b93c89
larroyo3 <lucas.arroyo@epitech.eu>
2026-03-27 12:21:16 +0100

    HttpClientFactory

---
+++ core/data/src/commonMain/kotlin/fr/acyll/core/data/logging/KermitLogger.kt
@@ -0,0 +1,22 @@
+package fr.acyll.core.data.logging
+
+import co.touchlab.kermit.Logger
+import fr.acyll.core.domain.logging.ChirpLogger
+
+object KermitLogger: ChirpLogger {
+    override fun debug(message: String) {
+        Logger.d(message)
+    }
+
+    override fun info(message: String) {
+        Logger.i(message)
+    }
+
+    override fun warn(message: String) {
+        Logger.w(message)
+    }
+
+    override fun error(message: String, throwable: Throwable?) {
+        Logger.e(message, throwable)
+    }
+}
