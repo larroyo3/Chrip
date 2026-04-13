c2a7d6307b00b8ae1306cf19a2b8e0b49bfae4d4
larroyo3 <lucas.arroyo@epitech.eu>
2026-04-13 19:41:44 +0200

    Adaptive Result Layout

---
+++ core/designsystem/src/commonMain/kotlin/fr/acyll/core/designsystem/components/layouts/ChirpAdaptiveResultLayout.kt
@@ -0,0 +1,99 @@
+package fr.acyll.core.designsystem.components.layouts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fr.acyll.core.designsystem.components.brand.ChirpBrandLogo
+import fr.acyll.core.designsystem.theme.ChirpTheme
+import fr.acyll.core.presentation.util.DeviceConfiguration
+import fr.acyll.core.presentation.util.currentDeviceConfiguration
+
+@Composable
+fun ChirpAdaptativeLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+   val configuration = currentDeviceConfiguration()
+
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
+        if (configuration == DeviceConfiguration.MOBILE_PORTRAIT) {
+            ChirpSurface(
+                modifier = Modifier
+                    .padding(innerPadding),
+                header = {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    ChirpBrandLogo()
+                    Spacer(modifier = Modifier.height(32.dp))
+                },
+                content = content
+            )
+        } else {
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(32.dp)
+            ) {
+                if (configuration != DeviceConfiguration.MOBILE_LANDSCAPE) {
+                    ChirpBrandLogo()
+                }
+
+                Column(
+                    modifier = Modifier
+                        .widthIn(max = 480.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(32.dp))
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(24.dp)
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.spacedBy(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ChirpAdaptativeLayoutPhonePortraitPreview() {
+    ChirpTheme {
+        ChirpAdaptativeLayout(
+            modifier = Modifier
+                .fillMaxSize(),
+            content = {
+                Text(
+                    text = "Registration successful!",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        )
+    }
+}
