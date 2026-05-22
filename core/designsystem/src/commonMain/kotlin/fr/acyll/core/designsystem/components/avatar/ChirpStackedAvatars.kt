e756a87deb6ec633bd6c436f9cdd5872bec57168
larroyo3 <lucas.arroyo@epitech.eu>
2026-05-22 18:55:05 +0200

    Stacked Avatars UI

---
+++ core/designsystem/src/commonMain/kotlin/fr/acyll/core/designsystem/components/avatar/ChirpStackedAvatars.kt
@@ -0,0 +1,78 @@
+package fr.acyll.core.designsystem.components.avatar
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.acyll.core.designsystem.theme.ChirpTheme
+
+@Composable
+fun ChirpStackAvatars(
+    avatars: List<AvatarUi>,
+    modifier: Modifier = Modifier,
+    size: AvatarSize = AvatarSize.SMALL,
+    maxVisible: Int = 2,
+    overlapPercentage: Float = 0.4f
+) {
+    val overlapOffset = -(size.dp * overlapPercentage)
+
+    val visibleAvatars = avatars.take(maxVisible)
+    val remainingCount = (avatars.size - maxVisible).coerceAtLeast(0)
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(overlapOffset),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        visibleAvatars.forEach { avatarUi ->
+            ChirpAvatarPhoto(
+                displayText = avatarUi.initials,
+                size = size,
+                imageUrl = avatarUi.imageUrl
+            )
+        }
+
+        if (remainingCount > 0) {
+            ChirpAvatarPhoto(
+                displayText = "$remainingCount+",
+                size = size,
+                textColor = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+}
+
+@Composable
+@Preview
+fun ChirpStackAvatarsPreview() {
+    ChirpTheme {
+        ChirpStackAvatars(
+            avatars = listOf(
+                AvatarUi(
+                    id = "1",
+                    username = "Philipp",
+                    initials = "PH"
+                ),
+                AvatarUi(
+                    id = "2",
+                    username = "John",
+                    initials = "JO"
+                ),
+                AvatarUi(
+                    id = "3",
+                    username = "Lucas",
+                    initials = "LA"
+                ),
+                AvatarUi(
+                    id = "3",
+                    username = "Romane",
+                    initials = "RB"
+                )
+            )
+        )
+    }
+}
